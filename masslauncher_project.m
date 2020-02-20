eaac60153dee3f80df264fedf36bc666b3820597
Chase Thomas <cj.thomas19@students.bchigh.edu>
2020-02-19 22:46:41 -0500

    sdfkhkhslda;f

--- masslauncher_project.m
+++ masslauncher_project.m
@@ -1,53 +1,58 @@
 function masslauncher_project
+clear all;
+close all;
 
-% test
-findlaunchvel(1,1,1,1)
+findlaunchvel(3,3,0.1,15,15,15);
 
 end
 
-function launchvel = findlaunchvel(m1,m2,k1,k2)
+function launchvel = findlaunchvel(w1,w2,w3,k1,k2,k3)
 
-g = 9.8;
-l1 = 1;
-l2 = 1;
-h1 = 1;
-h2 = 1;
+m1 = w1 * 0.03108095;
+m2 = w2 * 0.03108095;
+m3 = w3 * 0.03108095;
 
-init_w=[0;-sqrt(-2*g*(l1+l2+h1+h2-38));0;-sqrt(-2*g*(l1+l2+h1+h2-38))]; %initial conditions; expression for v1 and v2 generated with 'Big 5' eqns
+init_w = [0;-1;0;-1;0;-1];
 
 tspan=[0,10];
 options = odeset('Event',@(t,w) launchevent(t,w));
-[times,sol]=ode45(@(t,w) diffeq(t,w,m1,m2,k1,k2,g),tspan,init_w,options);
+[times,sol]=ode45(@(t,w) diffeq(t,w,m1,m2,m3,k1,k2,k3,g),tspan,init_w,options);
 
 
-plot(times,sol(:,3)); %Position of the topmost mass
+plot(times,sol(:,5)); %Position of the topmost mass
 hold on
-plot(times,sol(:,4));
+plot(times,sol(:,6));
 legend('Position of Top Mass','Velocity of Top Mass');
-launchvel=sol(end,4);
+launchvel=sol(end,6);
 disp(launchvel);
 
 
 
 end
 
-function dwdt = diffeq(t,w,m1,m2,k1,k2,g)
+function dwdt = diffeq(t,w,m1,m2,m3,k1,k2,k3,g)
 
 x1 = w(1);
 v1 = w(2);
 x2 = w(3);
 v2 = w(4);
+x3 = w(5);
+v3 = w(6);
 
-dv1dt=(-m1*g-k2*(x2-x1)+k1*x1)/m1;
-dv2dt=(-m2*g+k2*(x2-x1))/m2;
+%% TODO: Finish writing diff-eq
+dv1dt= (k2 * (x2 - x1) - m1 * g - k1 * x1) / m1;
+dv2dt= (k3 * (x3 - x2) - k2 * (x2 - x1) - m2 * g)/m2;
+dv3dt = (-k3 * (x3 - x2) - m2 * g)/m3;
 dx1dt=v1;
 dx2dt=v2;
-dwdt=[dx1dt;dv1dt;dx2dt;dv2dt]; %sets up the two differential equations
+dx3dt=v3;
+
+dwdt=[dx1dt;dv1dt;dx2dt;dv2dt;dx3dt;dv3dt]; %sets up the two differential equations
 
 end
 
 function [e_val, stop_val, e_dir] = launchevent(t,w)
-e_val = w(3);
+e_val = w(5);
 e_dir = 1;
 stop_val = 1;
 end %sets up the option to stop 
