dc8088a63d7f29a492bcda16b3f99c75e4ff03e9
sperangu <srinaath_perangur@brown.edu>
2020-02-19 23:34:49 -0500

    Update masslauncher_project.m

--- masslauncher_project.m
+++ masslauncher_project.m
@@ -2,22 +2,23 @@ function masslauncher_project
 clear all;
 close all;
 
-findlaunchvel(3,3,0.1,15,15,15);
+findlaunchvel(3,1,0.1,15,10,5);
 
 end
 
 function launchvel = findlaunchvel(w1,w2,w3,k1,k2,k3)
 
+g=386;
+
 m1 = w1 * 0.03108095;
 m2 = w2 * 0.03108095;
 m3 = w3 * 0.03108095;
 
-init_w = [0;-1;0;-1;0;-1];
+init_w = [0;-20;0;-20;0;-20];
 
 tspan=[0,10];
 options = odeset('Event',@(t,w) launchevent(t,w));
 [times,sol]=ode45(@(t,w) diffeq(t,w,m1,m2,m3,k1,k2,k3,g),tspan,init_w,options);
-
 
 plot(times,sol(:,5)); %Position of the topmost mass
 hold on
@@ -25,8 +26,6 @@ plot(times,sol(:,6));
 legend('Position of Top Mass','Velocity of Top Mass');
 launchvel=sol(end,6);
 disp(launchvel);
-
-
 
 end
 
@@ -53,6 +52,6 @@ end
 
 function [e_val, stop_val, e_dir] = launchevent(t,w)
 e_val = w(5);
-e_dir = 1;
 stop_val = 1;
+e_dir = 1;
 end %sets up the option to stop 
